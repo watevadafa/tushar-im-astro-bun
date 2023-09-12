266a5849dfbde9e9895fb211e1b97074568e1c43
Tushar Sarang <28387128+watevadafa@users.noreply.github.com>
2023-09-12 19:55:36 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://tushar.im',
   integrations: [mdx(), sitemap()],
   output: "server",
   adapter: cloudflare()
